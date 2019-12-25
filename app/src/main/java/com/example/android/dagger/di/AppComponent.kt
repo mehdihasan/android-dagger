50f5b64fd211b3faa202ca8afd903c7c57dc271b
Mehdi Hasan <email2mehdihasan@gmail.com>
2019-12-25 20:54:58 +0100

    registration and home page is now working

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,24 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationActivity
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+
+@Singleton
+@Component(modules = [StorageModule::class])
+interface AppComponent {
+
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindsInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
+    fun inject(activity: RegistrationActivity)
+    fun inject(activity: MainActivity)
+}
